33bddb23e30dc23d6f5b0df410770c3c654ca94f
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-10-05 11:16:38 +0000

    zrobiono zad 1-3

---
+++ KartaPracy2.py
@@ -0,0 +1,21 @@
+#zad 1
+# a = int(input("Podaj liczbe "))
+# if a % 3 == 0:
+#   print("Tak")
+# else :
+#   print("NIE")
+
+#zad 2
+# a = int(input("Zapodaj liczbę: "))
+# if a>=100 and a<1000 and a%17==0:
+#   print("TAK")
+# else: 
+#   print("Nie")
+
+#zad 3 
+
+wiek = int(input("Podaj wiek: "))
+if wiek>=18:
+ print("jesteś osobą pełnoletnią! BRAWO! A teraz Uciekaj")
+else:
+  print("Jesteś nie pełnoletni! Marsz do domu")
