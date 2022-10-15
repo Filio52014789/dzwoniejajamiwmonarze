60769ddbd57934b947eb47f29853222ccf9d1841
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-10-15 16:14:27 +0000

    zrobiono zadanie od 1-5

---
+++ KartaPracy2a.py
@@ -0,0 +1,54 @@
+#zad 1
+# a = int(input())
+# b = int(input())
+# if (a + b) % 2 == 0:
+#   print("Tak")
+# else :
+#   print("Nie")
+
+#zad 2
+# a = int(input())
+# g = int(input())
+# if (a+g)/2 > (a**g)/(1/2) :
+#   print("Tak")
+# else:
+#   print("Nie")
+
+#Zad 3
+# k = str(input())
+# l = str(input())
+# m = str(input())
+# if k== l and m : 
+#   print("Tak")
+# else :
+#   print("Nie")
+#zad 4
+# a = str(input())
+# b = str(input())
+# c = str(input())
+# d = str(input())
+# if a <= b and a <= c and a <= d :
+#   print("Najmniejsza z nich to: " +a)
+# if b <= a and b <= c and b <= d :
+#   print("Najmiejsza z nich to:" +b)
+# if c <= a and c <= b and c <= d :
+#   print("Najmiejsza z nich to:" +c)
+# if d <= a and d <= b and d <= c :
+#   print("Najmiejsza z nich to:" +d)
+
+#zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a<(b+c) or b<(a+c) or c<(b+a) :
+#   print("Tak")
+# else: 
+#   print("Nie")
+
+#Zad 6
+a = int(input())
+b = int(input())
+c = int(input())
+if a*a+b*b==c*c :
+  print("Trójkąt prostokątny")
+if 
