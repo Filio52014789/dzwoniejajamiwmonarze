c274f748dd8e6f84d05a54421a3b551ae783c05b
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-10-19 11:19:59 +0000

    zrobiono zad 4

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,6 +1,7 @@
- #pętla liczb dwucyfrowych od 10 do 21
+#PRE
+#pętla liczb dwucyfrowych od 10 do 21
 # for i in range(10, 22): print(i, end=" ")
-#   # pętla liczb dwucyfrowych nieparzystych od 15 do 31
+#pętla liczb dwucyfrowych nieparzystych od 15 do 31
 # for i in range(15,33,2): print(i, end=" ")
 # #pętla liczb dwucyfrowych maleją
 # for i in range(99,9,-1): print(i, end=" ")
@@ -18,3 +19,33 @@
 #Zad 2
 # for i in range(105,1000,15) :
 #   print(i, end=" ")
+
+# PRE 2
+# pętle for liczb trzycyfrowych podzielnych przez 13 
+# for i in range(104, 999, 13):
+#   print(i, end=" ")
+#Pętle for liczb dwucyfrowych parzystych 
+# for i in range(10,100,2):
+#   print(i, end=" ")
+#Pętle for potęg cyfr: 0, 1, 4, 9, 16, .. 81
+# for i in range(10):
+#   print(i*i, end=" ")
+
+#Zad 3
+# n = int(input())
+# for i in range(1, n+1) :
+#   if n % i == 0 :
+#     print(i, end=" ")
+#Zad 4
+# suma = 0
+# for i in range(10, 100):
+#   suma = suma + i
+# print(suma)
+
+#DOD - suma liczb trzycyfrowych parzystych 
+# suma = 0
+# for i in range(100,1000,2):
+#   suma = suma + i 
+# print(suma)
+
+#Zad 5
