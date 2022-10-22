754c7aa29d67726bf773b3df0cb657fdaa876671
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-10-22 12:22:17 +0000

    zrobiono zad 1-4

---
+++ KartaPracy3b.py
@@ -0,0 +1,23 @@
+#zad 1
+# for i in range(1, 31) :
+#   print(i, end=" ")
+
+#zad 2
+# n = int(input())
+# for i in range(n) :
+#   print(i**2, end=" ")
+
+#zad 3
+# for i in range(1137,10000,379):
+#   print(i, end=" ")
+
+#zad 4
+# for i in range(100,1000,5):
+#   print(i, end=" ")
+#   print("\n", "\n")
+# for y in range(102,1000,6):
+#   print(y, end=" ")
+#   print("\n")
+# for x in range(110,1000,11):
+#   print(x, end=" ")
+#   print("\n", "\n")
