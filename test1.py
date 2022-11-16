d0168400f704bab57c5ccfada50c266780b5ab86
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-11-16 12:20:15 +0000

    dodano folder KartyPracy

---
+++ test1.py
@@ -0,0 +1,14 @@
+k = int(input())
+l = int(input())
+m = int(input())
+if k == l:
+  print("Tak")
+  print(k, l)
+elif l == m:
+  print("Tak")
+  print(l, m )
+elif k == m:
+  print("Tak")
+  print(k, m)
+else:
+  print("Nie")
