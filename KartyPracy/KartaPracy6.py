b6545fcac3204d868349ff98c1d8a611958b1df3
1b15FM <64779077+Filio52014789@users.noreply.github.com>
2022-12-14 12:19:26 +0000

    zrobiono do 7 zad

---
+++ KartyPracy/KartaPracy6.py
@@ -0,0 +1,93 @@
+#Zad 3
+
+# for i in range(99,9,-1):
+#     if i % 7 == 0:
+#         podzielnik = i
+#         break
+    
+# ilosc = 0
+# for i in range(1000, 10000):
+#     if i % podzielnik == 0:
+#         ilosc += 1
+# print(ilosc)
+
+# #Zad 4
+
+# ilosc = 0
+# for i in range(10,100):
+#     cd = i // 10
+#     cj = i % 10
+#     if cd >= 2*cj:
+#         ilosc = ilosc + 1
+# print(ilosc)
+
+#Zad 5
+# print((456 // 10) % 10)
+
+#zad 6
+# 19 38 57 76 95 
+
+
+#ocja 1 
+# n = int(input())
+# licznik = 0 
+# suma = 0
+# i = 10
+# while True :
+#   if i % 19 == 0:
+#     suma = suma + i 
+#     licznik = licznik + 1
+#   if licznik == n:
+#     break
+#   i = i + 1
+# print(suma)
+
+
+#opcja 2
+
+# n = int(input())
+# licznik = 0
+# suma = 0
+# for i in range(10,100):
+#   if i % 19 == 0:
+#     suma = suma + i
+#     licznik = licznik + 1
+#   if licznik == n :
+#     break
+# print(suma)
+
+
+#opcja 3
+# n = int(input())
+# suma = 0
+# for i in range(19,19*n + 1, 19):
+#   suma = suma + i
+# print(suma)
+
+#opcja 4
+# n = int(input())
+# suma = 0
+# for i in range(1, n+1):
+#   suma = suma + 19*i
+# print(suma)
+
+#zad 7
+#opcja 1
+# n = int(input())
+# suma = 0
+# for i in range(999-(999 % 37), 999-(999 % 37) -(37*n) , -37):
+#   suma = suma + i
+# print(suma)
+
+# print(999-(999 % 38))
+
+#opcja 2
+n = int(input())
+suma = 0 
+licznik = 0
+for i in range(999,99,-1):
+  if i % 37 == 0:
+    suma = suma + i
+  if licznik == n:
+      break
+print(suma)
